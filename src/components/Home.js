9cee4b3891440dd056d8eae95c7f31c686beab38
keeforever <108993409+keeforever@users.noreply.github.com>
2022-11-07 20:04:35 +0530

    refactor: removed underline

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,7 @@ const Home = () => {
   return (
     <section className="app-container">
       <h1 className="text-center">Stock Images</h1>
-      <div>
-        <div className="underline"></div>
-      </div>
+
       <SearchBar />
       <Gallery />
     </section>
